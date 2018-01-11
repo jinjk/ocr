0ca1b132dc84b710d859ad9bd38825647f975302
Jiankun Jin <jiankun.jin@gmail.com>
2018-01-11 11:17:21 +0800

    update deploy only for static files

--- imreader-client/deploy.sh
+++ imreader-client/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-src_dir="$HOME/workspace/ocr/imreader-client/dist"
-dest_dir="$HOME/workspace/ocr/form-reader/src/main/resources/static"
-# cd ~/workspace/ocr/imreader-client; ng b --prod
-cp -r "$dest_dir/sample" "$src_dir/" && \
-rm -r "$dest_dir" && \
-cp -r "$src_dir" "$dest_dir" && \
-cd ~/workspace/ocr/form-reader/; mvn package -DskipTests && \
-echo 'jDXAtKgktPB9WrLG' | scp target/form-reader-0.0.1-SNAPSHOT.jar ubuntu@139.199.90.34:/home/ubuntu/
+dest_dir="$HOME/workspace/ocr/imreader-client/dist"
+cd ~/workspace/ocr/imreader-client; ng b --prod
+cd $dest_dir; zip -r dist.zip ./
+scp dist.zip ubuntu@139.199.90.34:/home/ubuntu/
+ssh ubuntu@139.199.90.34 "sudo rm -rf /var/www/html/* && sudo unzip dist.zip -d /var/www/html"
--- imreader-client/src/environments/environment.prod.ts
+++ imreader-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  imageUrl: "/imreader/api/v0.1/image",
-  sampleUrl: "/imreader/api/v0.1/image/sample",
-  baseUrl: ""
+  imageUrl: "http://139.199.90.34:8080/imreader/api/v0.1/image",
+  sampleUrl: "http://139.199.90.34:8080/imreader/api/v0.1/image/sample",
+  baseUrl: "http://139.199.90.34:8080"
 };
