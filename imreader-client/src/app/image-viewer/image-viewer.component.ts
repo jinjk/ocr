28dc7b06d0d2e27a0d9ceac41f5ac6a833b50909
Jiankun Jin <jiankun.jin@gmail.com>
2018-01-16 18:39:37 +0800

    add border on svg element

--- imreader-client/src/app/image-viewer/image-viewer.component.ts
+++ imreader-client/src/app/image-viewer/image-viewer.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, AfterViewInit, Input } from '@angular/core';
 import { ImageOcrService } from '../services/image-ocr.service';
 declare var jquery: any;
 declare var $: any;
+
+
+export enum Action {
+  editTable = 0,
+  editText = 1
+};
 
 @Component({
   selector: 'app-image-viewer',
   templateUrl: './image-viewer.component.html',
   styleUrls: ['./image-viewer.component.css']
 })
-export class ImageViewerComponent implements OnInit, OnChanges {
+export class ImageViewerComponent implements OnInit, OnChanges, AfterViewInit {
   @Input() viewerData: any;
   @Input() loading: boolean;
-  editingItem: any;
 
+  /**
+   * action is used to specify the text view editing status
+   * if action is 'editTable' then tableView will be activated, otherwise,
+   * if action is 'editText', text content editing will be enabled.
+   */
+  action: Action;
+  Action = Action;
+  svg = {
+    x: -10, y: -10, width: 0, height: 0,
+    toViewBox: () => {
+      return `${this.svg.x} ${this.svg.y} ${this.svg.width} ${this.svg.height}`;
+    }
+  };
+  editingItem: any;
   constructor() { }
 
   ngOnInit() {
@@ -22,9 +41,19 @@ export class ImageViewerComponent implements OnInit, OnChanges {
     this.initView();
   }
 
+  ngAfterViewInit() {
+    $("svg").click((event) => {
+      $("#cell_input").hide();
+    });
+  }
+
   public initView() {
+    this.action = Action.editText;
     $("#cell_input").hide();
-    $("svg").removeAttr("viewBox");
+  }
+
+  enableEditingTable() {
+
   }
 
   imageLoad() {
@@ -33,19 +62,22 @@ export class ImageViewerComponent implements OnInit, OnChanges {
     $img.removeAttr('width').removeAttr('height')
     let actualHeight = $img.height(), actualWidth = $img.width()
     $img.attr({ height: h, width: w }).data({ height: actualHeight, width: actualWidth })
-    console.debug(actualHeight, actualWidth);
-    $("svg").attr("viewBox", `-10 -10 ${Math.ceil(actualWidth * 1.2)} ${Math.ceil(actualHeight * 1.2)}`);
+    actualWidth = Math.ceil(actualWidth * 1.2); actualHeight = Math.ceil(actualHeight * 1.2);
+    this.svg.width = actualWidth;
+    this.svg.height = actualHeight;
   }
 
   editText(event, item) {
+    event = $.event.fix(event);
+    event.stopPropagation();
     let text = $(event.target);
     this.editingItem = item;
     let coor = text.offset();
-    let top = coor.top;
-    let left = coor.left;
+    let top = coor.top, left = coor.left, width = item.itemcoord.width;
+
     $("#cell_input").show();
     $("#cell_input").offset({ top: top, left: left });
-    $("#cell_input").width(item.itemcoord.width * 1.5);
+    $("#cell_input").width(width + Math.sqrt(width) * 1.8);
     $("#cell_input > input").val(item.itemstring);
     $("#cell_input > input").focus();
   }
