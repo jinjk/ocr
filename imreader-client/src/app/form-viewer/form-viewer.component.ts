dd3c12030fd1bb1a835d0ec073ad0fd63e0a3732
Jiankun Jin <jiankun.jin@gmail.com>
2018-01-18 18:05:27 +0800

    enable upload page

--- imreader-client/src/app/form-viewer/form-viewer.component.ts
+++ imreader-client/src/app/form-viewer/form-viewer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageOcrService } from '../services/image-ocr.service';
 import { ImageViewerComponent } from '../image-viewer/image-viewer.component';
+import { formDataMap } from '../test-data/form-data';
 declare var jquery:any;
 declare var $ :any;
 
@@ -11,11 +12,17 @@ declare var $ :any;
 })
 export class FormViewerComponent implements OnInit {
   viewerData: any;
+  formData: any;
 
   constructor(private imageOcrService: ImageOcrService) { }
 
   ngOnInit() {
     this.imageOcrService.imageSent((event: any) => {
+      //TODO this is test code, please create template for real world.
+      if (formDataMap.get("upload-page") == null) {
+        formDataMap.set("upload-page", {});
+      }
+      this.formData = formDataMap.get("upload-page");
       this.viewerData = {
         imageSrc: event.src,
         data: event.serverResponse.json().data
