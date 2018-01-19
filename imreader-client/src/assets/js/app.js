4c5ece154fbba27366af781487978de2402a037b
Jiankun Jin <jiankun.jin@gmail.com>
2018-01-19 13:26:20 +0800

    update table position

--- imreader-client/src/assets/js/app.js
+++ imreader-client/src/assets/js/app.js
@@ -6,8 +6,8 @@ $(document).on('click', '.navbar-collapse.in', function (e) {
     }
 });
 
-$(document).click(function(e) {
-    if($("#contextMenu").is(":visible")) {
+$(document).click(function (e) {
+    if ($("#contextMenu").is(":visible")) {
         $("#contextMenu").hide();
     }
 });
@@ -18,4 +18,37 @@ $(document).keydown(function (e) {
         $("#cell_input").hide();
     }
     return true;
+});
+let $window = $(window);
+let posStat = { greater: false};
+$window.scroll(function (e) {
+    let top = $window.scrollTop();
+    let imageContainer = $("#imageContainer");
+    let fieldsTable = $("#fieldsTable");
+
+
+    if (imageContainer != null) {
+        let pos = imageContainer.offset();
+        let height = imageContainer.height();
+        let threshold = pos.top + height - 150;
+        if (top > threshold && !posStat.greater) {
+            fieldsTable.css({
+                position: 'fixed',
+                top: pos.top
+            });
+        }
+        if (top <= threshold && posStat.greater) {
+            fieldsTable.css({
+                position: 'relative',
+                top: 0
+            });
+        }
+
+        if (top > threshold) {
+            posStat.greater = true;
+        }
+        else {
+            posStat.greater = false;
+        }
+    }
 });
